4d35e89331ccd1434ffaa081bd17ce8b2ebaa7b3
omar.r.c <omar.r.c@gmail.com>
2013-08-20 13:22:21 -0300

    Release

---
+++ DeduplicatePDF/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace DeduplicatePDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnDeDuplicate = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSrc = new System.Windows.Forms.TextBox();
+            this.txtDst = new System.Windows.Forms.TextBox();
+            this.btnSrc = new System.Windows.Forms.Button();
+            this.btnDst = new System.Windows.Forms.Button();
+            this.DlgFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.pgbFiles = new System.Windows.Forms.ProgressBar();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnDeDuplicate
+            // 
+            this.btnDeDuplicate.Location = new System.Drawing.Point(256, 126);
+            this.btnDeDuplicate.Name = "btnDeDuplicate";
+            this.btnDeDuplicate.Size = new System.Drawing.Size(141, 23);
+            this.btnDeDuplicate.TabIndex = 0;
+            this.btnDeDuplicate.Text = "DeDuplicate Pages";
+            this.btnDeDuplicate.UseVisualStyleBackColor = true;
+            this.btnDeDuplicate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pgbFiles);
+            this.groupBox1.Controls.Add(this.btnDst);
+            this.groupBox1.Controls.Add(this.btnDeDuplicate);
+            this.groupBox1.Controls.Add(this.btnSrc);
+            this.groupBox1.Controls.Add(this.txtDst);
+            this.groupBox1.Controls.Add(this.txtSrc);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(402, 200);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source Directory:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Destination Directory:";
+            // 
+            // txtSrc
+            // 
+            this.txtSrc.Location = new System.Drawing.Point(129, 22);
+            this.txtSrc.Name = "txtSrc";
+            this.txtSrc.Size = new System.Drawing.Size(237, 20);
+            this.txtSrc.TabIndex = 2;
+            // 
+            // txtDst
+            // 
+            this.txtDst.Location = new System.Drawing.Point(129, 61);
+            this.txtDst.Name = "txtDst";
+            this.txtDst.Size = new System.Drawing.Size(237, 20);
+            this.txtDst.TabIndex = 3;
+            // 
+            // btnSrc
+            // 
+            this.btnSrc.Location = new System.Drawing.Point(368, 22);
+            this.btnSrc.Name = "btnSrc";
+            this.btnSrc.Size = new System.Drawing.Size(28, 21);
+            this.btnSrc.TabIndex = 4;
+            this.btnSrc.Text = "...";
+            this.btnSrc.UseVisualStyleBackColor = true;
+            this.btnSrc.Click += new System.EventHandler(this.btnSrc_Click);
+            // 
+            // btnDst
+            // 
+            this.btnDst.Location = new System.Drawing.Point(369, 61);
+            this.btnDst.Name = "btnDst";
+            this.btnDst.Size = new System.Drawing.Size(28, 21);
+            this.btnDst.TabIndex = 5;
+            this.btnDst.Text = "...";
+            this.btnDst.UseVisualStyleBackColor = true;
+            this.btnDst.Click += new System.EventHandler(this.btnDst_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 163);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(426, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // pgbFiles
+            // 
+            this.pgbFiles.Location = new System.Drawing.Point(6, 97);
+            this.pgbFiles.Name = "pgbFiles";
+            this.pgbFiles.Size = new System.Drawing.Size(390, 23);
+            this.pgbFiles.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 185);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PDF Page Deduplicator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDeDuplicate;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnDst;
+        private System.Windows.Forms.Button btnSrc;
+        private System.Windows.Forms.TextBox txtDst;
+        private System.Windows.Forms.TextBox txtSrc;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog DlgFolder;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ProgressBar pgbFiles;
+    }
+}
+
---
+++ DeduplicatePDF/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace DeduplicatePDF
+{
+    public partial class Form1 : Form
+    {
+        private string src, dst;
+        private List<string> Files;
+        private int e, s;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void DeDuplicate()
+        {
+            e = s = 0;
+
+            pgbFiles.Value = 0;
+            pgbFiles.Maximum = Files.Count;
+            foreach (string File in Files)
+            {
+                try
+                {
+                    PdfReader pdfreader = new PdfReader(File);
+                    Dictionary<string, PdfImportedPage> pages = new Dictionary<string, PdfImportedPage>();
+                    Document document;
+                    MemoryStream stream = new MemoryStream();
+                    document = new Document(pdfreader.GetPageSizeWithRotation(1));
+                    PdfCopy pdfcopy = new PdfCopy(document, stream);
+                    document.Open();
+                    for (int i = 1; i <= pdfreader.NumberOfPages; i++)
+                    {
+
+                        string md5 = f_MD5(pdfreader.GetPageContent(i));
+                        if (!pages.ContainsKey(md5))
+                        {
+
+                            PdfImportedPage page;
+                            page = pdfcopy.GetImportedPage(pdfreader, i);
+                            pages.Add(md5, page);
+                            pdfcopy.AddPage(page);
+                        }
+
+                    }
+                    document.Close();
+
+                    string filename = newpath(File);
+                    FileStream fstream = new FileStream(filename, FileMode.Create);
+                    fstream.Write(stream.ToArray(), 0, stream.ToArray().Length);
+                    fstream.Close();
+                    s++;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error:" + ex.Message);
+                    e++;
+                }
+                pgbFiles.Value++;
+            }
+            if (e > 0)
+            {
+                MessageBox.Show("DeDuplicate done with erros");
+                
+            }
+            else
+            {
+                MessageBox.Show("DeDuplicate Done");
+            }
+            pgbFiles.Value = 0;
+        }
+
+        private string newpath(string s)
+        {
+            string filesrc = s.Replace(txtSrc.Text,"");
+            filesrc = txtDst.Text + filesrc;
+            string path = filesrc.Substring(0,filesrc.LastIndexOf('\\'));
+            //verify if path exists
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+
+            return filesrc;
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            src = txtSrc.Text.Trim();
+            dst = txtDst.Text.Trim();
+
+            if (src == "" || dst == "")
+            {
+                MessageBox.Show("Please, select a source and destination directory");
+            }
+            else
+            {
+                //find files
+                Files = new List<string>();
+                foreach (string file in Directory.EnumerateFiles(src, "*.pdf", SearchOption.AllDirectories))
+                {
+                    Files.Add(file);
+                }
+                if(Files.Count>0){
+                    if (MessageBox.Show(Files.Count + " Files found, deduplicate?", "", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        DeDuplicate();
+                    }
+                }else{
+                    MessageBox.Show("Files not found");
+                }
+            }
+        }
+
+
+        private string f_MD5(byte[] bytes)
+        {
+            MD5 md5 = MD5.Create();
+            byte[] hash = md5.ComputeHash(bytes);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+
+        private void btnSrc_Click(object sender, EventArgs e)
+        {
+            
+            if (DlgFolder.ShowDialog() == DialogResult.OK)
+            {
+                txtSrc.Text = DlgFolder.SelectedPath;
+            }
+        }
+
+        private void btnDst_Click(object sender, EventArgs e)
+        {
+            if (DlgFolder.ShowDialog() == DialogResult.OK)
+            {
+                txtDst.Text = DlgFolder.SelectedPath;
+            }
+        }
+    }
+}
